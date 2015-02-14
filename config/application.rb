b849151faea8d149e98ab4bcef468403d7a3c19f
MalcyL <malcolm@landonsonline.me.uk>
2015-02-14 16:03:44 +0000

    Tiniest Angular app ever.

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Receta
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
-
   end
 end
